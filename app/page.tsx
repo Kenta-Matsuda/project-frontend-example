235b1b5eb767f07ebd7f1e9829657e9c6b7b1150
Matsuda Kenta <matsuda.kenta@fujitsu.com>
2025-07-21 02:31:30 +0900

    ページ公開用修正

--- app/page.tsx
+++ app/page.tsx
@@ -125,7 +125,7 @@ const Page = () => {
         <AppBar position="static" sx={{ p: 1 }}>
           <Toolbar>
             <Typography variant="h1" component="h1">
-              日本語の都道府県別人口データ
+              [デモページ]日本語の都道府県別人口データ
             </Typography>
           </Toolbar>
         </AppBar>
